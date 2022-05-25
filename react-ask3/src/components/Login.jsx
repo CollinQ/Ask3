290ae4ca6c3964ee56c8da9f99d82604ad598476
CollinQ <collinqian@ucsb.edu>
2022-05-24 19:57:38 -0700

    login created

--- react-ask3/src/components/Login.jsx
+++ react-ask3/src/components/Login.jsx
@@ -1,41 +1,74 @@
 import React, { Component } from 'react'
 import { useState } from 'react';
-import "./pages.css"
 import axios from 'axios';
+import { useRef, useEffect } from 'react';
+
 
 
 
 
 const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
     const [email, setEmail] = useState();
     const [password, setPass] = useState();
+    const [errMsg, setErrMsg] = useState('');
+    const [success, getSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [email, password])
+
+
     return (
         <div className="login">
             <br></br>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive"> {errMsg}</p>
             <h1>Login
             </h1>
             <form>
-                <label>
+                <label htmlFor="email">
                     email:
-                    <input type="text"
-                        required
+                    <input
+                        type="text"
+                        id="email"
+                        ref={userRef}
+                        autoComplete="off"
+                        onChange={(e) => setEmail(e.target.value)}
                         value={email}
-                        onChange={(e) => setEmail(e.target.value)} />
-                </label>
-                <br></br>
-                <label>
-                    password:
-                    <input type="text"
                         required
-                        value={password}
-                        onChange={(c) => setPass(c.target.value)} />
+                    />
                 </label>
                 <br></br>
-                <input type="submit" value="Submit" />
+                <label htmlFor="password">
+                    password:
+                    <input
+                        type="password"
+                        id="password"
+                        onChange={(e) => setPass(e.target.value)}
+                        value={password}
+                        required
+                    />
+                    <br></br>
+                    <input type="submit" value="Sign In" />
+                </label>
             </form>
+            <br />
+            <p>
+                Need an Account?
+                <span className="line">
+                    {/*Sign up router link here*/}
+                    <a href="#"> Sign Up</a>
+                </span>
+            </p>
             {email} <br></br>
             {password}
-        </div>
+        </div >
     )
 }
 
--- react-ask3/src/components/Questions.jsx
+++ react-ask3/src/components/Questions.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import "./pages.css"
 
 export default class Questions extends Component {
     render() {
