64ef018df18edd1c921a112b2d0c8eb5a4a6c26f
CollinQ <collinqian@ucsb.edu>
2022-06-01 05:28:56 -0700

    Add Post

--- react-ask3/src/components/AddPost.jsx
+++ react-ask3/src/components/AddPost.jsx
@@ -2,26 +2,79 @@ import React, { Component } from 'react'
 import axios from '../api/axios'
 import { useState, useRef, useEffect } from 'react'
 
+const POST_URL = 'http://127.0.0.1:4000/post';
 
 const AddPost = () => {
     const userRef = useRef();
     const errRef = useRef();
 
+    const [subject, setSubject] = useState('');
     const [content, setContent] = useState('');
 
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            console.log("logging in");
+            const response = await axios.post(POST_URL,
+                { session: { subject: subject, content: content } },
+                {
+                    headers: {
+                        //'X-CSRF-Token': csrfToken,
+                        'Access-Control-Allow-Origin': 'http://127.0.0.1:4000/login',
+                        'Content-Type': 'application/json',
+                        'Access-Control-Response-Method': 'http://127.0.0.1:4000/login'
+                    }
+                });
+
+            setSubject('');
+            setContent('');
+        }
+
+        catch (err) {
+            errRef.current.focus();
+        }
+    }
+
     return (
-        <div>
-            addPost
-            <form>
+        <div className="addPost">
+            <br />
+            <h1>Submit Post</h1>
+            <form className='addPost-form' onSubmit={handleSubmit}>
                 <label>
                     subject:
                 </label>
-                <input>
-
-                </input>
-
+                <input
+                    placeholder="Subject Line"
+                    value={subject}
+                    type="subject"
+                    id="subject"
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setSubject(e.target.value)}
+                    required
+                />
+                <br />
+                <label>
+                    question:
+                </label>
+                <textarea
+                    as="textarea"
+                    placeholder="Write your question here..."
+                    style={{ height: "100px" }}
+                    value={content}
+                    type="content"
+                    id="content"
+                    ref={userRef}
+                    onChange={(e) => setContent(e.target.value)}
+                    required
+                />
+                <button type="submit"> Post Question </button>
             </form>
-
         </div>
     )
 }
