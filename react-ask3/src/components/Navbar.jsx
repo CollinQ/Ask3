f4eb024e7c03bf05d40bfda07be7cd07877ae67d
CollinQ <collinqian@ucsb.edu>
2022-06-01 01:10:29 -0700

    Jeff's Shit

--- react-ask3/src/components/Navbar.jsx
+++ react-ask3/src/components/Navbar.jsx
@@ -1,25 +1,65 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./Navbar.css";
 
+function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
 
-export default class Navbar extends Component {
-    render() {
-        return (
-            <div className="navbar">
-                <div className="logo">
-                    <img src="logo192.png" alt="poop" />
-                    <span className="ask">ask</span>
-                    <span className="num">3</span>
-                </div>
-                <ul className="menu">
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/questions'>Forum</Link></li>
-                    <li><Link to='/recent'>Recent</Link></li>
-                    <li><Link to='/profile'>Profile</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
-                    <li><Link to='/signup'>Signup</Link></li>
-                </ul>
-            </div >
-        )
-    }
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
+  return (
+    <nav className="nav">
+      <div className="logo">
+      <a href="/" className="link"><img src="logo192.png" alt="poop" /></a>
+        <span className="ask"><a href="/" className="link">ask</a></span>
+        <span className="num">3</span>
+      </div>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="/" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/questions" className="nav__link">
+            Forum
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/recent" className="nav__link">
+            Recent
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/profile" className="nav__link">
+            Profile
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/login" className="nav__link">
+            Login
+          </a>
+        </li>
+        <li className="nav__button">
+          <a href="/Signup" className="nav__link">
+            <button type="button">Sign up</button>
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+    </nav>
+  );
 }
+
+export default Navbar;
