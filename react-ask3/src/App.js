5ad88906034e52380bcdf939eff07e7e70b6d445
Clifford Xu <CliffordXu@Cliffords-Macbook.local>
2022-06-01 16:55:36 -0700

    transaction

--- react-ask3/src/App.js
+++ react-ask3/src/App.js
@@ -20,7 +20,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<><Home /><Bottom /><Text /><Boxes /></>} />
-          <Route path='/questions' element={<AddPost />} />
+          <Route path='/questions' element={<Questions />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Register />} />
         </Routes>
