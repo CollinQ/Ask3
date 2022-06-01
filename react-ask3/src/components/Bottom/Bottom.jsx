4ae9624d4d22d9afc3097d6b124169cc7c19fec4
Jeff Shen <jshen0303@gmail.com>
2022-06-01 15:30:04 -0700

    Update Bottom.jsx

--- react-ask3/src/components/Bottom/Bottom.jsx
+++ react-ask3/src/components/Bottom/Bottom.jsx
@@ -9,7 +9,7 @@ const WhatGPT3 = () => (
     <div className="Home_ask3-heading">
       <h1 className="text">Every Question Has An Answer</h1>
       <p>What is Ask3?</p>
-      <p2>Shaping people, shaping business</p2>
+      <p2>Ask3 is an incentivized question and answer forum that connects a community of knowledgeable individuals to eager learners. Answers are confirmed by users through an upvote and downvote system, allowing users who answer correctly to accumulate XP and gain credibility. In exchange, XP can be redeemed and rewarded through MetaMask, which encourages individuals to participate in forum discussions.</p2>
     </div>
 
     <div className="image">
