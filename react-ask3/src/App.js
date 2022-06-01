47b8c43bf5859f2ca3d445fef1f2b3e97834ed1b
CollinQ <collinqian@ucsb.edu>
2022-06-01 16:57:48 -0700

    small change

--- react-ask3/src/App.js
+++ react-ask3/src/App.js
@@ -20,7 +20,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<><Home /><Bottom /><Text /><Boxes /></>} />
-          <Route path='/questions' element={<AddPost />} />
+          <Route path='/questions' element={<Forum />} />
+          <Route path='/addpost' element={<AddPost />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Register />} />
         </Routes>
